74d5fbeb21f45ad84610777a872f4d13463ed8c5
karthi k <karthiideveloper@gmail.com>
2023-05-15 23:14:34 +0530

    temp

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,8 +1,8 @@
-// import handler from './api/user'
 
+import handler from './user'
 export default function Home() {
 
   const req={method:"GET"},res='';
-  // handler(req,res);
+  handler(req,res);
   return <h1>hi</h1>;
 }
---
+++ src/pages/api/user.js
@@ -0,0 +1,56 @@
+import { MongoClient } from "mongodb";
+
+// Connection URL and database name
+const uri =
+  "mongodb+srv://karthi11:karthi11@nextcrud.eurtma2.mongodb.net/?retryWrites=true&w=majority";
+const dbName="nextCRUD"
+// Create a MongoDB client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Connect to the MongoDB server
+client.connect();
+
+const handler=async(req, res) =>{
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      // Fetch all users
+      const users = await getUsers();
+      res.status(200).json(users);
+      break;
+    case "POST":
+      // Create a new user
+      const { name, email } = req.body;
+      const newUser = await createUser(name, email);
+      res.status(201).json(newUser);
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
+
+// Fetch all users
+async function getUsers() {
+  const db = client.db(dbName);
+  const collection = db.collection("users");
+  const users = await collection.find().toArray();
+  return users;
+}
+
+// Create a new user
+async function createUser(name, email) {
+  const db = client.db(dbName);
+  const collection = db.collection("users");
+  const newUser = { name, email };
+  const result = await collection.insertOne(newUser);
+  return result.ops[0];
+}
+
+
+export default handler
